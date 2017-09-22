1a140ce6be8c77f52fafb8c1f4ba2191cb96d586
fireattack <human.peng@gmail.com>
2017-09-21 23:51:17 -0500

    Forget to add onclick.

--- yandere_artist_highlight.user.js
+++ yandere_artist_highlight.user.js
@@ -3,22 +3,22 @@
 // @namespace   org.fireattack.yandere
 // @match       *://yande.re/post
 // @match       *://yande.re/post?*
-// @version     0.0.3
+// @version     0.0.4
 // ==/UserScript==
 
 function artistCheck() {
 
-    var taglist = document.querySelector('div.tag-completion-box').nextElementSibling.nextElementSibling;
-    var tagjson = taglist.innerText.match(/Post\.register_tags\((.+)\)/)[1];
+    var tagList = document.querySelector('div.tag-completion-box').nextElementSibling.nextElementSibling;
+    var tagJson = tagList.innerText.match(/Post\.register_tags\((.+)\)/)[1];
 
-    var tagobj = JSON.parse(tagjson);
+    var tagObj = JSON.parse(tagJson);
 
     var thumbs = document.querySelectorAll("a.thumb");
-    for (var index = 0; index < thumbs.length; ++index) {
-        var imgtags = thumbs[index].querySelector('img').title.match(/Tags: (.+) User/)[1];
+    for (let index = 0; index < thumbs.length; ++index) {
+        let imgtags = thumbs[index].querySelector('img').title.match(/Tags: (.+) User/)[1];
         imgtagsarrry = imgtags.split(' ');
-        for (var j = 0; j < imgtagsarrry.length; ++j) {
-            if (tagobj[imgtagsarrry[j]] === "artist") {
+        for (let j = 0; j < imgtagsarrry.length; ++j) {
+            if (tagObj[imgtagsarrry[j]] === "artist") {
                 thumbs[index].parentElement.style['box-shadow'] = "0px 0px 0px 5px #cccc00";
                 break;
             }
@@ -32,6 +32,7 @@ var node = document.createElement("a");
 node.href = "#";
 node.id = "artist-check";
 node.style="color:#cccc00";
+node.onclick = function(e){artistCheck();};
 node.appendChild(document.createTextNode('<Check artist>'));
 sidebar.appendChild(document.createElement('br'));
 sidebar.appendChild(node);
