7186baf7b451858f0dc327662812b7ffcb5d6101
fireattack <human.peng@gmail.com>
2017-10-10 21:07:12 -0500

    Rewrite using JQ's promise

--- yandere_additional_functionality.user.js
+++ yandere_additional_functionality.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name          Yande.re additional functionality
 // @namespace     org.fireattack.yandere
-// @description   
+// @description
 // @match         *://yande.re/*
 // @version       1.1
 // ==/UserScript==
@@ -12,6 +12,10 @@ function getCookie(name) {
   if (parts.length == 2) return parts.pop().split(";").shift();
 }
 
+function reloadPage() {
+	window.location.reload;
+}
+
 Object.size = function(obj) {
     var size = 0, key;
     for (key in obj) {
@@ -20,42 +24,44 @@ Object.size = function(obj) {
     return size;
 };
 
+function transferTagsPrepare(sourceID, targetID) {
 
-// TODO: 用update_batch重写
-function transferTags(sourceID, targetID, reload) {    
+    var deferred = jQuery.Deferred();
+	jQuery
+      .ajax({
+        url: "/post.json",
+        data: {
+          tags: "id:" + sourceID,
+        },
+        dataType: "json",
+      })
+	  .done(function(resp) {
+		  var tags = resp[0].rating + resp[0].tags;
+		  var toBeUpdated = [{id: targetID, tags: tags, old_tags: ""}];
+          deferred.resolve(toBeUpdated);
+	  });
 
-    var req = new XMLHttpRequest();
-    req.open("GET", "https://yande.re/post.json?tags=id:"+sourceID);
-    req.onload = function(){
-        let myJSON = JSON.parse(req.responseText);
-        var tags = myJSON[0]['rating'] + ' ' + myJSON[0]['tags'];
-		var toBeUpdate = [{id: targetID, tags: tags, old_tags: ""}];
-		if (reload) 
-			Post.update_batch(toBeUpdate, window.location.reload);
-		else 
-			Post.update_batch(toBeUpdate);
-        
-        // var request = new XMLHttpRequest();
-        // request.open("POST", "https://yande.re/post/update.xml");
-        // request.onload = function(){
-            // if (reload) windows.location.reload();
-        // }
-        
-        // var formData = new FormData();
-        // var username = getCookie('login');
-        // var pwHash = getCookie('pass_hash');  
-        // formData.append("login", username);
-        // formData.append("password_hash", pwHash); 
-        // formData.append("id", targetID); 
-        // formData.append("post[tags]", tags);     
-        // request.send(formData);  
-    }    
-    req.send();    
-}; 
+      return deferred.promise();
+};
+
+function batchTransferTagsToParent() {
+	var toBeUpdated = [], promises = [];
+    for (var id in Post.posts._object){
+        var post = Post.posts._object[id];
+        if (post.parent_id) {
+			var promise = transferTagsPrepare(id, post.parent_id).done(function(data){toBeUpdated.push(data);});
+            promises.push(promise);
+        }
+    }
+
+    jQuery.when.apply(jQuery, promises).done(function(){
+        Post.update_batch(toBeUpdated);
+        });
+}
 
 
 if (/post\/show/i.test(window.location.href)){
-	var currentID = window.location.href.match(/\d+/)[0];
+	var id = window.location.href.match(/\d+/)[0];
 	var statusNotice = document.querySelectorAll('.status-notice');
 	if (statusNotice){
 		for (let notice of statusNotice) {
@@ -64,37 +70,34 @@ if (/post\/show/i.test(window.location.href)){
 				let node = document.createElement('a');
 				node.href = '#';
 				node.textContent = '[Transfer tags]';
-				node.onclick= function(){transferTags(childID, currentID);}
-				notice.appendChild(node);   
+				node.onclick= function(){
+                    transferTagsPrepare(childID, id).done(function(data){
+                        Post.update_batch(data, reloadPage);
+                    })
+                    }
+				notice.appendChild(node);
 				break;
 			}
 		}
 	}
 }
 
-function batchTransferTagsToParent() { 
-    for (var id in Post.posts._object){
-        var post = Post.posts._object[id];
-        if (post.parent_id) {
-            transferTags(id, post.parent_id);
-        }
-    }    
-}
+
 
 function batchTransferPoolshipToParent(){
 
-    var toBeUpdate = [];	
-    
-    for (var id in Post.posts._object){
-        var post = Post.posts._object[id];        
-        if (post.parent_id && post.pool_posts && Object.size(post.pool_posts._object)===1) {
-			var poolID = post[Object.keys(post)[0]];		
-            toBeUpdate.push({id: id, tags: "-pool:" + poolID, old_tags: ""});
-            toBeUpdate.push({id: post.parent_id, tags: "pool:" + poolID + ":" + post.pool_posts._object[poolID].sequence, old_tags: ""});
-            //transfer_post2(myP, , 4668, );        
-        }
-    }    
+    var toBeUpdated = [];
 
-    Post.update_batch(toBeUpdate);
+    for (var id in Post.posts._object){
+        var post = Post.posts._object[id];
+        if (post.parent_id && post.pool_posts && Object.size(post.pool_posts._object)===1) {
+			var poolID = post[Object.keys(post)[0]];
+            toBeUpdated.push({id: id, tags: "-pool:" + poolID, old_tags: ""});
+            toBeUpdated.push({id: post.parent_id, tags: "pool:" + poolID + ":" + post.pool_posts._object[poolID].sequence, old_tags: ""});
+            //transfer_post2(myP, , 4668, );
+        }
+    }
+
+    Post.update_batch(toBeUpdated);
 }
 
