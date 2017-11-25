d1baca06e3ae14bcde220b4ff784de6748e34be8
fireattack <human.peng@gmail.com>
2017-11-25 17:22:45 -0600

    Fix

--- mltd_wiki_add_idol_colors_to_tables.user.js
+++ mltd_wiki_add_idol_colors_to_tables.user.js
@@ -22,13 +22,15 @@ angelNames = getNames('#js_oc_box_m2','yellow');
 names = princessNames.concat(fairyNames,angelNames);
 
 for (let span of document.querySelectorAll('table span')){
-    for (let name of names) {
-        if (span.textContent.includes(name[1])) {
-            //span.style.color = name[0];
-            span.style.cssText = `color: ${name[0]};`
-            if (name[0]!='blue') span.style.cssText += 'background-color: black;';             
-            break;
-        }
-    }        
+    if (!span.querySelector('a')) {
+        for (let name of names) {
+            if (span.textContent.includes(name[1])) {
+                //span.style.color = name[0];
+                span.style.cssText = `color: ${name[0]};`
+                if (name[0]!='blue') span.style.cssText += 'background-color: black;';             
+                break;
+            }
+        }        
+    }
 }
     
