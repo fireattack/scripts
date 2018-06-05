9c544f0976a4d0dfebe8cb9673011a44bd4a9e6c
fireattack <human.peng@gmail.com>
2018-06-04 22:14:04 -0500

    Pixiv new interface

--- pixiv_quick_fav.user.js
+++ pixiv_quick_fav.user.js
@@ -2,7 +2,7 @@
 // @name              Pixiv quick fav
 // @name:zh-CN        Pixiv 一键收藏
 // @namespace         https://twitter.com/ikenaikoto
-// @version           0.2
+// @version           1.0
 // @description       One-click fav on Pixiv
 // @description:zh-cn Pixiv 一键收藏，避免烦人的页面跳转
 // @author            fireattack
@@ -23,57 +23,76 @@ function urlencodeFormData(fd) { //From: https://stackoverflow.com/questions/754
     return s;
 }
 
-var favBtn = document.querySelector('.bookmark-container');
-if (!favBtn.querySelector('a:last-child').className.includes('bookmarked')){
-    
-    var sucess, loading, done;    
-    
-    switch (favBtn.querySelector('span:last-child').textContent) {
-      case '添加收藏':  //Chinese
-        success = '成功!';
-        loading = '收藏中...';
-        done = '编辑收藏';
-        break;
-      case 'ブックマークに追加':  //Japanese
-        success = '完成!';
-        loading = '読み込み中...';
-        done = 'ブックマークを編集';
-        break;
-      default:
-        success = 'Done!';
-        loading = 'Bookmarking..';
-        done = 'Edit Bookmark';
+var target = document.querySelector('body');
+
+// create an observer instance
+var observer = new MutationObserver(function (mutations, ob) {
+    mutations.forEach(function (mutation) {
+        var favBtn = document.querySelector('a[href*="/bookmark_add.php"]');
+        if (favBtn && favBtn.querySelectorAll('path')) {
+            main(favBtn);
+            //ob.disconnect();
+        }
+    });
+});
+
+// configuration of the observer:
+var config = {
+    attributes: true,
+    childList: true,
+    characterData: true
+};
+
+// pass in the target node, as well as the observer options
+observer.observe(target, config);
+
+function main(favBtn) {
+    color = window.getComputedStyle(favBtn.querySelectorAll('path')[0])['fill'];
+
+    if (color = "rgb(255, 255, 255)") {
+
+        favBtn.onclick = (event) => {
+            var data = new FormData();
+            data.append('mode', 'save_illust_bookmark');
+            data.append('illust_id', window.location.href.match(/id=(\d+)/)[1]);
+            data.append('restrict', '0');
+            data.append('comment', '');
+            data.append('tags', '');
+            data.append('tt', globalInitData.token);
+            data = urlencodeFormData(data);
+
+            var xhr = new XMLHttpRequest();
+            xhr.open('POST', '/rpc/index.php', true);
+            xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded; charset=UTF-8');
+            xhr.onload = () => {
+                var jsonResponse = JSON.parse(xhr.responseText);
+                if (jsonResponse.error)
+                    window.alert('Error!');
+                else {
+                    clearInterval(t);
+                    favBtn.querySelectorAll('path')[0].style.fill = 'rgb(255, 64, 96)';
+                    favBtn.querySelectorAll('path')[1].style.fill = 'rgb(255, 64, 96)';
+                }
+            };
+            xhr.send(data);
+
+            i = 255;
+            j = -10;
+            var t = setInterval(() => {
+                str = `rgb(${i}, 94, 96`;
+                favBtn.querySelectorAll('path')[1].style.fill = str;
+                i = i + j;
+                if (i < 0) {
+                    i = -i;
+                    j = -j;
+                };
+                if (i > 255) {
+                    i = 255 - (i - 255);
+                    j = -j;
+                }
+            }, 30);
+
+            event.preventDefault();
+        }
     }
-    
-    favBtn.onclick = (event) => {
-        var data = new FormData();
-        data.append('mode', 'save_illust_bookmark');
-        data.append('illust_id', pixiv.context.illustId);
-        data.append('restrict', '0');
-        data.append('comment', '');
-        data.append('tags', '');
-        data.append('tt', pixiv.context.token);
-        data = urlencodeFormData(data);
-
-        var xhr = new XMLHttpRequest();
-        xhr.open('POST', '/rpc/index.php', true);
-        xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded; charset=UTF-8');
-        xhr.onload = () => {
-            var jsonResponse = JSON.parse(xhr.responseText);
-            if (jsonResponse.error)
-                window.alert('Error!');
-            else favBtn.querySelector('span:last-child').textContent = success;
-            setTimeout(() => {
-                favBtn.querySelector('a').className = '_bookmark-toggle-button bookmarked edit-bookmark';
-                favBtn.querySelector('span:last-child').textContent = done;
-                favBtn.onclick = null;
-            }, 1000);
-        };
-
-        xhr.send(data);
-
-        favBtn.querySelector('span:last-child').textContent = loading;
-        event.preventDefault();
-    };    
 }
-
