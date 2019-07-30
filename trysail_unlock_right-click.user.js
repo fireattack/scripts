26976278cd0233c68f3d1e3981eb1c71ce4c004f
fireattack <human.peng@gmail.com>
2019-07-30 04:56:36 -0500

    update

--- trysail_unlock_right-click.user.js
+++ trysail_unlock_right-click.user.js
@@ -1,12 +1,36 @@
 // ==UserScript==
 // @name         Trysail.jp Unlock right-click
 // @namespace    https://twitter.com/ikenaikoto
-// @version      0.1
+// @version      0.2
 // @description  Get rekt Music Ray'n
 // @author       fireattack
 // @match        https://trysail.jp/*
+// @grant        GM_addStyle
 // ==/UserScript==
+
+/* Remove CSS bullshit */
+
+GM_addStyle(`
+.protect .img-protect {
+
+    user-select: auto !important;
+    -moz-user-select: auto !important;
+    -ms-user-select: auto !important;
+    -webkit-user-select: auto !important;
+    -khtml-user-select: auto !important;
+    -webkit-user-drag: auto !important;
+    -khtml-user-drag: auto !important;
+    -webkit-touch-callout: auto !important;
+}
+.protect .img-protect:after {
+    content: none !important;
+}
+
+`);
 
 /* Allow Copy */
 
 $("body").unbind('contextmenu');
+$(".protect-content").unbind('mousedown');
+$(".protect-content").unbind('selectstart');
+$(".protect-content").unbind('contextmenu');
