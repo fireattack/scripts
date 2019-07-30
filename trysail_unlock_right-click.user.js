3ff643de24edc7572c1fee97e25a948ae1d827d4
fireattack <human.peng@gmail.com>
2019-07-30 04:33:51 -0500

    +trysail_unlick_right-click

---
+++ trysail_unlock_right-click.user.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name         Trysail.jp Unlock right-click
+// @namespace    https://twitter.com/ikenaikoto
+// @version      0.1
+// @description  Get rekt Music Ray'n
+// @author       fireattack
+// @match        https://trysail.jp/*
+// ==/UserScript==
+
+/* Allow Copy */
+
+$("body").unbind('contextmenu');
