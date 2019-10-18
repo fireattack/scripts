14d76a63fae15ab736530e3d2138ef5c0b0c00ef
fireattack <human.peng@gmail.com>
2019-10-18 00:57:06 -0500

    Update

---
+++ bookwalker_yimeng.js
@@ -0,0 +1,11 @@
+var width=prompt("请输入打算保存图片宽度！","1600");
+var height=prompt("请输入打算保存图片高度！","1142");
+document.getElementById('renderer').style.width=width+'px';
+document.getElementById('renderer').style.height=height+'px';
+yy_y = null;
+Ily$ = null;
+LIe$ = null;
+$eCl$ = null;
+$LI = null;
+icilc = null
+alert('翼梦论坛 www.otomedream.com 提醒你右键菜单已经解除，可以对着图片右键另存为到本地了！');
