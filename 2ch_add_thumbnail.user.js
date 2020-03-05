ce1ae9c03403158d70a95d99fd001d16c806702e
fireattack <human.peng@gmail.com>
2020-03-05 02:17:26 -0600

    +2ch image show thumbnail

---
+++ 2ch_add_thumbnail.user.js
@@ -0,0 +1,49 @@
+// ==UserScript==
+// @name         2ch(5ch)サムネイル表示
+// @namespace    http://tampermonkey.net/
+// @version      2.0
+// @description  2(5)ちゃんねるに貼られている画像のサムネイルを表示します。
+// @author       ぬ / fireattack
+// @match        http://*.5ch.net/*
+// @match        https://*.5ch.net/*
+// @match        http://*.2ch.sc/*
+// @match        https://*.2ch.sc/*
+// @match        http://*.bbspink.com/*
+// @match        https://*.bbspink.com/*
+// @grant        none
+// @require      http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js
+// ==/UserScript==
+
+function addThumb() {
+    $('a').each(function(){
+      var address = $(this).text();
+      var tmp = address.split('.');
+      var ext = tmp[tmp.length-1];
+        if(ext=="jpg" ||ext=="jpeg" || ext=="png" || ext=="gif" || ext=="bmp" || ext=="jpg:large" || ext=="jpeg:large" || ext=="png:large"){
+      $(this).after($('</br><a href='+address+' target="_blank"><img src='+address+' width=400/></a></br>'));
+      }
+    });
+  
+   $(".thumb_i").each(function(){
+       $(this).hide();
+    });
+  }
+  
+  addThumb(); // Add once first so our condition later is true initially
+  
+  var target = document.querySelector('body > dl');
+  
+  // create an observer instance
+  var observer = new MutationObserver(function(mutations, ob) {
+      mutations.forEach(function(mutation) {
+        if(!document.querySelector('picture > a > img')) {
+            addThumb();
+        }
+      });
+  });
+  
+  // configuration of the observer:
+  var config = { attributes: true, childList: true, characterData: true };
+  
+  // pass in the target node, as well as the observer options
+  observer.observe(target, config);
