e69903b1cddcbd227c1490c59d7236e92ac42e4d
fireattack <human.peng@gmail.com>
2020-05-13 02:43:16 -0500

    Use MutationObserver to make sure allowCopy is applied.

--- trysail_unlock_right-click.user.js
+++ trysail_unlock_right-click.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Trysail.jp Unlock right-click
 // @namespace    https://twitter.com/ikenaikoto
-// @version      0.3
+// @version      0.4
 // @description  Get rekt Music Ray'n
 // @author       fireattack
 // @match        https://trysail.jp/*
@@ -9,9 +9,10 @@
 // @grant        GM_addStyle
 // ==/UserScript==
 
-/* Remove CSS bullshit */
+(function () {
 
-GM_addStyle(`
+    /* Remove CSS bullshit */
+    GM_addStyle(`
 .protect .img-protect {
 
     user-select: auto !important;
@@ -23,7 +24,6 @@ GM_addStyle(`
     -khtml-user-drag: auto !important;
     -webkit-touch-callout: auto !important;
 }
-
 .protect .img-protect img {
     user-select: auto !important;
     -moz-user-select: auto !important;
@@ -39,9 +39,29 @@ GM_addStyle(`
 }
 `);
 
-/* Allow Copy */
+    function allowCopy() {
+        /* Allow Copy */
+        //$("#image-and-banners").remove();
+        //$("body").unbind('load');
+        $("body").unbind('contextmenu');
+        $(".protect-content").unbind('mousedown');
+        $(".protect-content").unbind('selectstart');
+        $(".protect-content").unbind('contextmenu');
+    }
 
-$("body").unbind('contextmenu');
-$(".protect-content").unbind('mousedown');
-$(".protect-content").unbind('selectstart');
-$(".protect-content").unbind('contextmenu');
+    // Remove loading and adjust cover
+    $('.main-visual-item .img-responsive').height(200);
+    $('.main-visual-item .img-responsive').width('auto');
+    $("#jsLoading").remove();
+
+    var observer = new MutationObserver(function (mutations, ob) {
+        mutations.forEach(function (mutation) {
+            console.log('Re-adding allowCopy..');
+            allowCopy();
+        });
+    });
+
+    var config = { attributes: true };
+
+    observer.observe(document.body, config);
+})();
