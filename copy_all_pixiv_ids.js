0967f382080d4101d8801657149b8bd71782fd63
fireattack <human.peng@gmail.com>
2020-08-12 18:13:15 -0500

    Update

---
+++ copy_all_pixiv_ids.js
@@ -0,0 +1,18 @@
+const copyToClipboard = str => {
+    const el = document.createElement('textarea');
+    el.value = str;
+    document.body.appendChild(el);
+    el.select();
+    document.execCommand('copy');
+    document.body.removeChild(el);
+};
+var IDs = [];
+for (var a of document.querySelectorAll('a')) {
+    if (/artworks\/\d+/.test(a.href)) {
+        IDs.push(a.href.match(/artworks\/(\d+)/)[1]);        
+    }
+}
+
+IDs = [...new Set(IDs)];
+var s = IDs.join(' ');
+copyToClipboard(s);
