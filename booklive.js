f0ac7d3e297f9757380e28d592c3da1a45ad68b7
fireattack <human.peng@gmail.com>
2020-08-30 13:45:53 -0500

    Update booklive

--- booklive.js
+++ booklive.js
@@ -3,34 +3,36 @@ function downloadPage(pageNo) {
     let imgs = document.querySelectorAll(`#content-p${pageNo} > div > div > img`);
 
     /* 
-    The deocoded images are loaded into three <img>s and then attached to each other vertially.
-    But you can't directly merge them together, as there are overlaps betwen 1/2 and 2/3. 
+    The decoded images are loaded into three <img>s and then attached to each other vertically.
+    But you can't directly merge them together, as there are overlaps between 1/2 and 2/3. 
     THe overlapping, or even the img size of three parts, are not the same or consistent within 
     the same book.
     
     However, the displayed y-coordinate, or offset, of part 2 (y1) reveals the actual 
-    visible height of part 1. we can get that height in original dimension (sectionHeight) 
+    visible height of part 1. we can get that height in original dimension (Y1) 
     by scaling it back using the ratio between displayed width (w0) and original width of <img>.
 
-    So, the part we want to keep for part 1 is from 0 to sectionHeight (the rest are overlapped by
-    part 2 so no need to keep).
+    So, the part we want to keep for part 1 is from 0 to Y1 (the rest are overlapped by
+    part 2 so no need to keep). 
     
-    This sectionHeight is the same for part 2 (since y2 = y1 *2). So we want to keep the exactly same 
-    height, from 0 to sectionHeight (the rest are overlapped by part 3).
+    For part 2, do the same. So we want to keep the height, from 0 to Y2 (the rest are overlapped by 
+    part 3).
 
     For part 3 we just keep the whole height of <img>, since nothing is overlapping on it.
     */
     let y1 = imgs[1].parentElement.style.cssText.match(/translate\([[0-9.]+px, *([0-9.]+)px\);$/)[1];
-    let w0 = imgs[0].parentElement.style.width.replace('px', '');
-    let sectionHeight = Math.round(y1 / w0 * imgs[0].width);
+    let y2 = imgs[2].parentElement.style.cssText.match(/translate\([[0-9.]+px, *([0-9.]+)px\);$/)[1];
+    let w0 = imgs[0].parentElement.style.width.replace('px', ''); //w should be all the same.
+    let Y1 = Math.round(y1 / w0 * imgs[0].width);
+    let Y2 = Math.round(y2 / w0 * imgs[0].width);
 
     let c = document.createElement('canvas');
     c.width = imgs[0].width;
-    c.height = sectionHeight * 2 + imgs[2].height;
+    c.height = Y2 + imgs[2].height;
     let ctx = c.getContext("2d");
     ctx.drawImage(imgs[0], 0, 0);
-    ctx.drawImage(imgs[1], 0, sectionHeight);
-    ctx.drawImage(imgs[2], 0, sectionHeight * 2);
+    ctx.drawImage(imgs[1], 0, Y1);
+    ctx.drawImage(imgs[2], 0, Y2);
     c.toBlob((blob) => {
         let a = document.createElement('a');
         a.download = `${pageNo}.png`;
