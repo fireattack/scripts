16fb73ec42f6e4599530b3f6960349405e4e5f78
fireattack <human.peng@gmail.com>
2021-04-08 22:08:33 -0500

    Update

--- bookmarklets/download_image.js
+++ bookmarklets/download_image.js
@@ -1,7 +1,16 @@
 var my_str = [];
-[...document.querySelectorAll('img')].map(ele => {
-    if (ele.src.includes('image.')) {
+
+var selector = 'img';
+
+[...document.querySelectorAll(selector)].map(ele => {
+    if (ele.src) {
         my_str.push(ele.src);
+    }
+    else if (ele.style.backgroundImage) {
+        my_str.push(new URL(
+            ele.style.backgroundImage.replace(/url\(\"(.+)@.+?\"\)/, '$1'),
+            window.location.href).href
+        );
     }
 });
 console.log(my_str.join('\n'));
