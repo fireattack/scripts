fa320dd770eb0a559630248268e7e3a1594e3dd8
fireattack <human.peng@gmail.com>
2022-02-11 08:18:18 -0600

    vgmdb: now replacing str using intermediate variables

--- vgmdb_helper.user.js
+++ vgmdb_helper.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name             VGMdb Liner Note Helper
 // @namespace        http://tampermonkey.net/
-// @version          1.0
+// @version          2.0
 // @description      Within album notes in VGMdb, link to artists, and reference tracks by name
 // @author           fireattack
 // @original-author  btown
@@ -13,7 +13,7 @@
 
 // modified from https://openuserjs.org/scripts/btown/VGMdb_Liner_Note_Helper as the author no longer updates it
 
-const DEBUGGING = false;
+const DEBUGGING = true;
 
 function restoreConsole() {
     var i = document.createElement('iframe');
@@ -21,32 +21,48 @@ function restoreConsole() {
     document.body.appendChild(i);
     window.console = i.contentWindow.console;
 }
+
+function escapeRegExp(text) {
+    return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, '\\$&');
+}
 restoreConsole();
 var notes = document.getElementById('notes');
 var html = notes.innerHTML;
+
+
+var toBeReplaced = [];
 var links = document.querySelectorAll('a[href^="/artist/"]');
 [...links].map((link) => {
     try {
         var linkText = link.querySelector('[lang="en"]') || link;
         var name = linkText.textContent;
+        var re = new RegExp(escapeRegExp(name), 'g');
         var nameJa = (link.querySelector('[lang="ja"]') || link).textContent;
-        var replacement = `<a href="${link.href}" target="_blank">${nameJa}</a>`;
-        DEBUGGING && console.log('Name is', name);
-        DEBUGGING && console.log(replacement);
-        if (notes.innerHTML.includes(replacement)) return;
-        for (node of notes.childNodes) {
-            if (node.nodeType == 3 && node.textContent.includes(name) && !/M-\d+/.test(node.textContent)) {
-                DEBUGGING && console.log('Find matching node', node);
-                var newNode = document.createElement("span");
-                newNode.innerHTML = node.textContent.replace(new RegExp(name, 'g'), replacement);
-                node.replaceWith(newNode);
-            }
-        }
+        var repl = `<a href="${link.href}" target="_blank">${nameJa}</a>`;
+        toBeReplaced.push({
+            re: re,
+            repl: repl,
+        });   
     }
     catch (err) { }
 });
 
+// replace with placeholder so links won't be broken 
+for (idx in toBeReplaced) {
+    DEBUGGING && console.log(re, repl);
+    var {re, repl} = toBeReplaced[idx];
+    var placeHolder = `___${idx}___`;
+    html = html.replace(re, placeHolder);
+}
+
+for (idx in toBeReplaced) {
+    var {re, repl} = toBeReplaced[idx];
+    var placeHolder = `___${idx}___`;
+    html = html.replace(new RegExp(placeHolder, 'g'), repl);
+}
+
 // Feature: add track name to note.
+var toBeReplaced = [];
 var tracks = document.querySelectorAll('.tl tr.rolebit');
 [...tracks].map((track) => {
     try {
@@ -54,18 +70,10 @@ var tracks = document.querySelectorAll('.tl tr.rolebit');
         var num = cols[0].textContent.trim();
         var title = cols[1].textContent.trim();
         DEBUGGING && console.log(num, title);
-        var re = new RegExp(`(\\w-)?\\b(${num}|${parseInt(num, 10)})\\b`, 'g')
-        for (node of notes.childNodes) {
-            if (node.nodeType == 3 && re.test(node.textContent)) {
-                DEBUGGING && console.log('Find matching node', node);
-                var newNode = document.createElement("span");
-                newNode.innerHTML = node.textContent.replace(re, `$& (${title})`);
-                node.replaceWith(newNode);
-            }
-        }
+        var re = new RegExp(`(\\w-)?\\b(${num}|${parseInt(num, 10)})\\b`, 'g');
+        var repl = `$& (${title})`;
+        html = html.replace(re, repl);
     }
     catch (err) { }
 });
-
-
-
+notes.innerHTML = html;
