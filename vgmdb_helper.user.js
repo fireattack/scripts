44973ecba259210fe251936d535e1e9908f76d4c
fireattack <human.peng@gmail.com>
2022-08-02 06:21:36 -0500

    Update

--- vgmdb_helper.user.js
+++ vgmdb_helper.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name             VGMdb Liner Note Helper
 // @namespace        http://tampermonkey.net/
-// @version          2.2
+// @version          2.3
 // @description      Within album notes in VGMdb, link to artists, and reference tracks by name
 // @author           fireattack
 // @original-author  btown
@@ -62,7 +62,7 @@ for (idx in toBeReplaced) {
 
 // Feature: add track name to note.
 var toBeReplaced = [];
-var tracks = [...document.querySelectorAll('div#tracklist table.role:last-of-type .tl tr.rolebit')];
+var tracks = [...document.querySelectorAll('div#tracklist table.role:last-of-type tr.rolebit')];
 [...tracks].map((track) => {
     try {
         var cols = track.children;
