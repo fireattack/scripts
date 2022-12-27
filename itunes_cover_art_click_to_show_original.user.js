182de78ab280855c155241037ff2a8e575fd2d5a
fireattack <human.peng@gmail.com>
2022-12-27 21:16:42 +0800

    Update itunes cover

--- itunes_cover_art_click_to_show_original.user.js
+++ itunes_cover_art_click_to_show_original.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         iTunes Cover Art Click to Show Original
 // @namespace    https://twitter.com/ikenaikoto
-// @version      1.2
+// @version      2.0
 // @description  Click on cover art to get original largest PNG image.
 // @author       fireattack
 // @match        *://itunes.apple.com/*/album/*
@@ -9,7 +9,7 @@
 // @match        *://music.apple.com/*/artist/*
 // ==/UserScript==
 
-const DEBUGGING = false;
+const DEBUGGING = true;
 let config = { attributes: true, childList: true, characterData: true, subtree: false };
 let target = document.querySelector('html');
 let observer = new MutationObserver(function (mutations, ob) {
@@ -25,8 +25,8 @@ function print() {
 
 function addLink() {
   print('Start!');
-  observer.observe(document.querySelector('div.loading-inner'), config);
-  let picture = document.querySelector('div.product-info picture');
+  // observer.observe(document.querySelector('div.loading-inner'), config);
+  let picture = document.querySelector('div.artwork > div.artwork-component picture');
   if (!picture) return;
   let source = picture.querySelector('source');
   let url = '';
@@ -35,23 +35,25 @@ function addLink() {
   } else {
     url = source['src'];
   }
-  url = url.replace(/\/[0-9]*x[0-9]*[a-z]*(?:-[0-9]+)?(\.[^/.]*)$/, "/999999999x0w-999.png");
+  let orig_url = url.replace(/^.+?mzstatic\.com\/image\/thumb\/([^/]+\/+v4\/+(?:[a-f0-9]{2}\/+){3}[-0-9a-f]{20,}\/[^/]+)\/*[^/]*$/, "https://a1.mzstatic.com/us/r1000/063/$1");
   let img = picture.querySelector('img');
   if (!img) return;
-  img.style.cursor = 'pointer';
+
   let thumbnail_url = '';
   if (img.src && !img.src.includes('1x1.gif')) {
     return;
   } else if (img.currentSrc && !img.currentSrc.includes('1x1.gif')) {
     thumbnail_url = img.currentSrc;
   } else {
-    thumbnail_url = url.replace("/999999999x0w-999.png", "/500x500bb.webp");
+    thumbnail_url = url.replace(/\/[0-9]*x[0-9]*[a-z]*(?:-[0-9]+)?(\.[^/.]*)$/, "/500x500bb.webp");
   }
   print('Update img.src:', img.src, thumbnail_url);
   img.src = thumbnail_url;
+  img.style.cursor = 'pointer';
+  img.style.opacity = 1;
 
   if (img.parentNode.nodeName !== "A") {
-    img.outerHTML = "<a href='" + url + "' target='_blank'>" + img.outerHTML + "</a>";
+    img.outerHTML = "<a id='test' href='" + orig_url + "' target='_blank'>" + img.outerHTML + "</a>";
     let link = document.querySelector('picture img').parentNode; // get the link we just added using outerHTML
     link.onclick = function (e) {
       e.stopPropagation();
@@ -61,5 +63,5 @@ function addLink() {
 }
 
 observer.observe(target, config);
-observer.observe(document.querySelector('div.loading-inner'), config);
+// observer.observe(document.querySelector('div.loading-inner'), config);
 //addLink();
